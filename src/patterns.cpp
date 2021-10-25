0b806ee3274ae0364fd0204ddcd496ee2ce44dfb
kthays <kthays23@gmail.com>
2021-10-25 13:40:20 -0600

    Initial commit for pattern module

---
+++ src/patterns.cpp
@@ -0,0 +1,68 @@
+#include "patterns.h"
+
+#define PATTERN_STR_LEN 16  // 8.3 files
+
+// ** Pattern
+Pattern::Pattern(const char* _szcFilePath, int _iTimePerRowMS)
+: pNext(nullptr)
+, iTimePerRowMS(_iTimePerRowMS)
+{
+    strlcpy(szcFilePath, _szcFilePath, PATTERN_STR_LEN);
+}
+
+const char* Pattern::GetFilePath() const
+{
+    return szcFilePath;
+}
+
+int Pattern::GetTimePerRowMS() const
+{
+    return iTimePerRowMS;
+}
+
+
+// ** CyclicPatternList
+CyclicPatternList::CyclicPatternList()
+: pFirst(nullptr)
+, pLast(nullptr)
+, pCurrent(nullptr)
+{
+
+}
+
+CyclicPatternList::~CyclicPatternList()
+{
+    // Delete all patterns in the list
+    while (pFirst != nullptr) {
+        pCurrent = pFirst->pNext;
+        delete pFirst;
+        pFirst = pCurrent;
+    } 
+}
+
+// Takes ownership of the given pattern and appends it to the end of the list
+void CyclicPatternList::AddPattern(Pattern* pPattern)
+{
+    // Special case where the list is empty0
+    if (pFirst == nullptr) {
+        pFirst = pPattern;
+        pCurrent = pPattern;
+        pLast = pPattern;
+        return;
+    }
+
+    // Append the new pattern to the end of the list
+    if (pLast != nullptr) pLast->pNext = pPattern;
+    pLast = pPattern;
+}
+
+const Pattern* CyclicPatternList::GetCurrentPattern() const
+{
+    return pCurrent;
+}
+
+void CyclicPatternList::Advance()
+{
+    if (pCurrent != nullptr) pCurrent = pCurrent->pNext;
+    if (pCurrent == nullptr) pCurrent = pFirst; // Loop back around
+}
---
+++ src/patterns.h
@@ -0,0 +1,35 @@
+
+
+// ** Pattern - Metadata for a pattern, includes the file path on the SD card to the pattern data, and how fast it should go
+class Pattern {
+public:
+    Pattern(const char* szcFilePath, int iTimePerRowMS);
+
+    const char* GetFilePath() const;
+    int GetTimePerRowMS() const;
+
+private:
+    friend class CyclicPatternList; // Give the list container access to the pNext var
+    Pattern* pNext;
+
+    char* szcFilePath;
+    int iTimePerRowMS;
+};
+
+// ** CyclicPatternList - A linked list of Pattern objects that loops back around to the first element after the last element
+class CyclicPatternList {
+public:
+    CyclicPatternList();
+    ~CyclicPatternList();
+
+    // Takes ownership of the given pattern and appends it to the end of the list
+    void AddPattern(Pattern* pPattern);
+
+    const Pattern* GetCurrentPattern() const;
+    void Advance(); // Advance the current pattern to the next in the list
+
+private:
+    Pattern* pFirst;
+    Pattern* pLast;
+    Pattern* pCurrent;
+};
